69be5b02e0ab4f978a4f94619132ee72c1043ad8
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-01-10 02:26:24 -0500

    Export community config as a file

--- src/community.ts
+++ src/community.ts
@@ -2,9 +2,9 @@ export = {
     // Order of extends is critical: community config shadows all rules,
     // so overrides for @perfective/eslint-config need to be applied again.
     extends: [
-        '../',
+        './',
         '@perfective/eslint-config/community',
-        '../rules/eslint-config',
+        './rules/eslint-config',
     ],
     rules: {
         'react/jsx-indent-props': ['warn', 0],
