5d5cdc5a281965f3c877072ca3a5e6164000895b
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-02-14 13:46:37 -0500

    Remove unnecessary overrides from the project’s ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,3 @@
 module.exports = {
     extends: ['@perfective/eslint-config'],
-    overrides: [
-        {
-            files: ['*.js'],
-            rules: {
-                'unicorn/prevent-abbreviations': 'off',
-            },
-        },
-        {
-            files: ['*.ts'],
-            parserOptions: {
-                project: './tsconfig.json',
-            },
-            settings: {
-                'import/resolver': {
-                    typescript: {
-                        project: 'tsconfig.json',
-                    },
-                },
-            },
-            rules: {
-                'unicorn/prevent-abbreviations': 'off',
-            },
-        },
-        {
-            files: ['*.spec.ts'],
-            settings: {
-                jest: {
-                    version: 26,
-                },
-            },
-        },
-    ],
 };
