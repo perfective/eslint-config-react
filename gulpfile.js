91bd47cbc27db6038e18e8c088062dd72ad03022
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-03-28 00:57:36 -0400

    Build the project with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,31 @@
+const del = require('del');
 const gulp = require('gulp');
-const gulpJsonEditor = require('gulp-json-editor');
 
-function copyPackageJson() {
+const gulpAsciidoctor = require('@asciidoctor/gulp-asciidoctor');
+const gulpJsonEditor = require('gulp-json-editor');
+const gulpRename = require('gulp-rename');
+const gulpTypeScript = require('gulp-typescript');
+
+function typeScriptConfig(config, settings = {}) {
+    const project = gulpTypeScript.createProject(config, settings);
+    return project.src().pipe(project());
+}
+
+function build() {
+    return typeScriptConfig('tsconfig.build.json')
+        .pipe(gulp.dest('./dist'));
+}
+
+exports.build = build;
+
+function clean(callback) {
+    del(['./dist', '*.tsbuildinfo']);
+    callback();
+}
+
+exports.clean = clean;
+
+function packageJson() {
     return gulp
         .src('./package.json')
         .pipe(gulpJsonEditor({
@@ -14,13 +38,13 @@ function copyPackageJson() {
                 'rules.d.ts',
                 'rules/**/rules/*.d.ts',
             ],
+            devDependencies: undefined,
+            scripts: undefined,
         }))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./dist'));
 }
 
-exports.copyPackageJson = copyPackageJson;
-
-function copyStaticFiles() {
+function staticFiles() {
     return gulp
         .src([
             './LICENSE',
@@ -29,12 +53,25 @@ function copyStaticFiles() {
             './README.md',
             './src/**/package.json',
         ])
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./dist'));
 }
 
-exports.copyStaticFiles = copyStaticFiles;
+function documentation() {
+    return gulp.src('./src/index.adoc')
+        .pipe(gulpAsciidoctor())
+        .pipe(gulpRename(path => {
+            path.basename = 'docs';
+            return path;
+        }))
+        .pipe(gulp.dest('dist'));
+}
+
+exports.documentation = documentation;
 
 exports.default = gulp.series(
-    copyPackageJson,
-    copyStaticFiles,
+    clean,
+    build,
+    packageJson,
+    staticFiles,
+    documentation,
 );
