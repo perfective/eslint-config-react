5a93b2f20e67198e57cd91afa59759b4a4abbd84
Andrey Mikheychik <a.mikheychik@gmail.com>
2026-01-10 14:38:48 -0600

    Inline `eslintConfig()` into `perfectiveEslintReactConfig()`

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { Linter } from 'eslint';
 
 import { jestFiles, perfectiveEslintConfig, typescriptFiles } from '@perfective/eslint-config';
 
-import { eslintConfig } from './rules/eslint-config/eslint-config';
+import { typescriptEslintNamingConvention } from './rules/eslint-config/rules/typescript-eslint';
+import { unicornPreventAbbreviations } from './rules/eslint-config/rules/unicorn';
 import { jsxA11yConfig } from './rules/jsx-a11y/jsx-a11y-config';
 import { reactConfig } from './rules/react/react-config';
 import { reactHooksConfig } from './rules/react-hooks/react-hooks-config';
@@ -14,7 +15,29 @@ export function perfectiveEslintReactConfig(configs: Linter.Config[] = []): Lint
         reactConfig,
         reactHooksConfig,
         reactPerfConfig,
-        eslintConfig,
+        {
+            rules: {
+                '@stylistic/indent': ['warn', 4, {
+                    // TODO: Research more options using https://astexplorer.net
+                    // eslint-disable-next-line @typescript-eslint/naming-convention -- option name
+                    SwitchCase: 1,
+                    ignoredNodes: [
+
+                        /**
+                         * @workaround https://github.com/typescript-eslint/typescript-eslint/issues/455#issuecomment-580636221
+                         */
+                        'TSTypeParameterInstantiation',
+                    ],
+                }],
+                'unicorn/prevent-abbreviations': ['warn', unicornPreventAbbreviations()],
+            },
+        },
+        {
+            files: typescriptFiles,
+            rules: {
+                '@typescript-eslint/naming-convention': ['error', ...typescriptEslintNamingConvention()],
+            },
+        },
         {
             files: typescriptFiles,
             // Review
