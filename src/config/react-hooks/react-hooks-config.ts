0f271366a7a53db14274251ebfcbc4c428a33d74
Andrey Mikheychik <a.mikheychik@gmail.com>
2026-01-10 22:12:43 -0600

    Upgrade eslint-plugin-react-hooks to v7.0.1
    - Enable `react-hooks/component-hook-factories` as an error.
    - Enable `react-hooks/config` as an error.
    - Enable `react-hooks/error-boundaries` as an error.
    - Enable `react-hooks/gating` as an error.
    - Enable `react-hooks/globals` as an error.
    - Enable `react-hooks/immutability` as an error.
    - Enable `react-hooks/incompatible-library` as an error.
    - Enable `react-hooks/preserve-manual-memoization` as an error.
    - Enable `react-hooks/purity` as an error.
    - Enable `react-hooks/refs` as an error.
    - Enable `react-hooks/set-state-in-effect` as an error.
    - Enable `react-hooks/set-state-in-render` as an error.
    - Enable `react-hooks/static-components` as an error.
    - Enable `react-hooks/unsupported-syntax` as an error.
    - Enable `react-hooks/use-memo` as an error.

--- src/config/react-hooks/react-hooks-config.ts
+++ src/config/react-hooks/react-hooks-config.ts
@@ -1,5 +1,5 @@
 import { Linter } from 'eslint';
-import { configs } from 'eslint-plugin-react-hooks';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 
 import { javascriptFiles, typescriptFiles } from '@perfective/eslint-config';
 
@@ -7,11 +7,28 @@ export function reactHooksConfig(): Linter.Config {
     return {
         files: [...javascriptFiles, ...typescriptFiles],
         plugins: {
-            'react-hooks': configs['recommended-latest'].plugins['react-hooks'],
+            'react-hooks': reactHooksPlugin,
         },
         rules: {
             'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'warn',
+            'react-hooks/exhaustive-deps': 'error',
+            // Compiler hooks
+            // React Compiler rules
+            'react-hooks/component-hook-factories': 'error',
+            'react-hooks/config': 'error',
+            'react-hooks/error-boundaries': 'error',
+            'react-hooks/gating': 'error',
+            'react-hooks/globals': 'error',
+            'react-hooks/immutability': 'error',
+            'react-hooks/incompatible-library': 'error',
+            'react-hooks/preserve-manual-memoization': 'error',
+            'react-hooks/purity': 'error',
+            'react-hooks/refs': 'error',
+            'react-hooks/set-state-in-effect': 'error',
+            'react-hooks/set-state-in-render': 'error',
+            'react-hooks/static-components': 'error',
+            'react-hooks/unsupported-syntax': 'error',
+            'react-hooks/use-memo': 'error',
         },
-    };
+    } as Linter.Config;
 }
